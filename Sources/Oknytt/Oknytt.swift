de23eccb945ba5d84d0ce1add01c53fb64a22ed1
Mikael Hultgren <blomma@artsoftheinsane.com>
2024-01-28 14:30:57 +0100

    Initial commit

---
+++ Sources/Oknytt/Oknytt.swift
@@ -0,0 +1,31 @@
+import os
+import Foundation
+
+extension Logger {
+    private static var subsystem = Bundle.main.bundleIdentifier!
+
+    /// Logs the payment flows like Apple Pay.
+    @available(OSX 11.0, iOS 14.0, *)
+    static let debug = Logger(subsystem: subsystem, category: "DEBUG")
+}
+
+public func DLog(
+    message: String,
+    view: String = "",
+    file: StaticString = #file,
+    function: StaticString = #function,
+    line: UInt = #line,
+    column: UInt = #column
+) {
+    #if DEBUG
+    Logger.debug.debug(
+        "🟩 \(file) : \(view) : \(function) : \(line) : \(column) - \(message) 🟩")
+    #endif
+}
+
+// get current dispatch queue label
+extension DispatchQueue {
+    static var currentLabel: String {
+        return String(validatingUTF8: __dispatch_queue_get_label(nil)) ?? "unknown"
+    }
+}
